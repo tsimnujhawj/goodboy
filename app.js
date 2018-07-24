fc61689b4fca5adc4ae1438ac28807e6de9100ce
tsimnujhawj <tsimnuj.hawj@gmail.com>
2018-07-24 18:04:35 -0500

    fix

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const mysql = require("mysql");
 
+// Connection settings
 let con = mysql.createConnection({
     host: "localhost",
     user: "root",
@@ -8,9 +9,21 @@ let con = mysql.createConnection({
     port: 3306,
 })
 
+// Connect
 con.connect(function(error){
     if (error) throw error;
     console.log("connected!");
-    connection.end();
+    displayItems();
 })
 
+// Display all of the items avaliable for purchase
+function displayItems(){
+con.query("SELECT * FROM products", (error, results) => {
+    if (error) throw error;
+    for (let i = 0; i < results.length; i++) {
+        console.log(results[i].id + " | " + results[i].product_name + " | " + results[i].price + " | " + results[i].stock_quantity);
+    }
+    console.log("-----------------------------------");
+    con.end();
+})
+}
