d23ed48f235ad652037b896d235c5e6ceac0ace7
tsimnujhawj <tsimnuj.hawj@gmail.com>
2018-07-25 15:25:41 -0500

    updated selection and added ascii art

--- app.js
+++ app.js
@@ -21,16 +21,49 @@ con.connect((error)=>{
 function displayItems(){
 con.query("SELECT * FROM products", (error, results)=> {
     if (error) throw error;
+    console.log("|--------------------------------------------------|");
+    console.log("|---------------Good Boy! Good Buy!----------------|");
+    console.log("|--------------------------------------------------|");
+    console.log("   _____ _____  ___________  ______  _______   __")
+    console.log("  |  __ \\  _  ||  _  |  _  \\ | ___ \\|  _  \\ \\ / /")
+    console.log("  | |  \\/ | | || | | | | | | | |_/ /| | | |\\ V / ")
+    console.log("  | | __| | | || | | | | | | | ___ \\| | | | \\ /  ")
+    console.log("  | |_\\ \\ \\_/ /\\ \\_/ / |/ /  | |_/ /\\ \\_/ / | |  ")
+    console.log("   \\____/\\___/  \\___/|___/   \\____/  \\___/  \\_/  ")
+    console.log(" ");
+    console.log("|--------------------------------------------------|");
+    console.log("|--------------------------------------------------|");
+    console.log("|--------------------------------------------------|");      
+
     for (let i = 0; i < results.length; i++) {
         console.log(results[i].id + " | " + results[i].product_name + " | " + results[i].price + " | " + results[i].stock_quantity);
     }
     console.log("-------------------------------------------");
     inquirer.prompt({
-        name: "confirm",
-        type: "confirm",
+        name: "options",
+        type: "list",
         message: "Do you want to buy something?",
-    }).then({
-        if ()
+        choices: ["YES", "NO"]
+    }).then(answer =>{
+        switch (answer.options) {
+            case "YES":
+                buyItem();
+                break;
+            case "NO":
+                noBuy();
+                break;
+            default:
+                displayItems();
+        }
     })
 })
+}
+
+function buyItem(){
+    console.log("You wanna buy.");
+}
+
+function noBuy(){
+    console.log("y u no buy?");
+    con.end();
 }
